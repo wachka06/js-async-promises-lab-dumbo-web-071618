54e322cef6434b35caaf3c13126e7a7dcc870387
wachka06 <wachka06@gmail.com>
2018-09-04 09:37:00 -0400

    not finished

--- index.js
+++ index.js
@@ -5,3 +5,52 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question){
+  const qContainer = document.querySelector('.question-container');
+  qContainer.innerHTML = question.questionText;
+}
+
+function askQuestionThen(time){
+  question = questions[0]
+  appendQuestion(question)
+  toggleTrueAndFalseButtons()
+  return new Promise((resolve, reject) => {
+   setTimeout(function(){
+     resolve(question)
+   }, time )
+ })
+}
+
+function removeQuestion(promise) {
+  console.log(promise);
+  const qContainer = document.querySelector('.question-container');
+  qContainer.innerHTML = ''
+  toggleTrueAndFalseButtons()
+  return new Promise((resolve, reject) => {
+    resolve()
+ })
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  askQuestionThen(time)
+  .then(removeQuestion)
+}
+
+function trueAndFalseButtons(){
+  const trueButton = document.querySelector('#true-btn');
+  const falseButton = document.querySelector('#false-btn');
+  return [trueButton, falseButton];
+}
+
+function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach(function(button){
+    button.classList.toggle("hide")
+  })
+}
+
+function displayQuestionOnClick(){
+  askQuestionThenRemoveQuestion(5000)
+
+
+}
